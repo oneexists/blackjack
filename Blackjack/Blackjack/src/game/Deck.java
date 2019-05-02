a8d7a395afe259eaf2bd4ebea72c2ee6a894fd42
oneexists <47866455+oneexists@users.noreply.github.com>
2019-05-02 13:57:05 -0500

    Add files via upload
    April

--- Blackjack/Blackjack/src/game/Deck.java
+++ Blackjack/Blackjack/src/game/Deck.java
@@ -1,20 +1,95 @@
+/*
+ * endGame method
+ */
+
+
 package game;
+
+import java.util.Random;
+import java.util.Scanner;
 
 public class Deck {
 	public static final int CARDS = 52;
-	private Card deckOfCards = new Card();
-	private int currentCard;
-	
-	
+	private boolean[] inDeck = new boolean[CARDS];
+	Card[] deck = new Card[CARDS];
+	Random r = new Random();
+	private int currentCard = 0;
+	PlayerHand player = new PlayerHand();
+	DealerHand dealer = new DealerHand();
+	public static boolean split = false;
 	
 	//methods
-		//dealGame
-	public Card deal(int rank, int suit) {
-		
+		//get next card
+	public int getNextCard(){
+		int i = r.nextInt(52);
+		while(inDeck[i]) {
+		    i = r.nextInt(52);
+		}
+		inDeck[i] = true;
+		return i;
 	}
-		//shuffle
-		//dealCard
-		//aceValue
-
-
-}}
+	
+		//create the deck
+	public Card[] deck() {
+		for (int i = 0; i < CARDS; ++i) {
+		    Card j = new Card(getNextCard());
+		    deck[i] = j;
+		}
+		return deck;
+	}
+	
+		//deal the card
+	public Card dealCard() {
+		Card card = deck[currentCard];
+		currentCard++;
+		return card;
+	}
+	
+		//deal the game
+	public void dealGame() {
+		//initialize deck
+		this.deck();
+		//deal cards
+		player.addCard(dealCard());
+		dealer.addCard(dealCard());
+		player.addCard(dealCard());
+		dealer.addCard(dealCard());	
+	}
+	
+		//split hand
+	public void splitHand(Card splitCard, int cardPosition) {
+		PlayerHand splitHand = new PlayerHand();
+		splitHand.addCard(splitCard);
+		player.removeCard(splitCard, cardPosition);
+		split = true;	
+	}
+		
+		/*
+		 * splitHand.getHandSum() doesn't work
+		 * floating else for all hands over 21?
+		 * implement GUI
+		//end game 
+	public String endGame() {
+		if (split == false) {
+			if (dealer.getHandSum() > player.getHandSum()) {
+				//dealer wins
+				return("Dealer wins.");
+			}
+			else if (dealer.getHandSum() < player.getHandSum()) {
+				//player wins
+				return("You win!");
+			}
+		}
+		else {
+			if (dealer.getHandSum() > player.getHandSum() 
+				&& dealer.getHandSum() > splitHand.getHandSum()) {
+					//dealer wins
+					return("Dealer wins.");
+				}
+			else if (player.getHandSum() > dealer.getHandSum()
+					|| splitHand.getHandSum() > dealer.getHandSum()) {
+						//player wins
+						return("You win!");
+			}
+	}*/
+}
--- Blackjack/Blackjack/src/game/PlayerHand.java
+++ Blackjack/Blackjack/src/game/PlayerHand.java
@@ -1,5 +1,78 @@
+/*
+ * implement endGame to addCard method
+ */
 package game;
 
 public class PlayerHand {
-
+	private int numCards = 0;
+	private int handSum = 0;
+	private int aces = 0;
+	private Card[] hand = new Card[20];
+	
+	//constructors
+	public PlayerHand() {
+		this.numCards = 0;
+		this.handSum = 0;
+		this.aces = 0;
+	}
+	
+	//methods
+		//getters and setters
+	public int getHandSum() {
+		for (int i = 0; i < this.numCards; i++) {
+			if (hand[i].getAce() == true) {
+				aces++;
+			}
+			else {
+				handSum += hand[i].getValue();
+			}
+			
+			while (handSum > 21 && aces > 0) {
+				handSum -= 10;
+				aces--;
+			}
+		}
+		return handSum;
+	}
+	
+		//add card
+	public int addCard(Card aCard) {
+		this.hand[numCards] = aCard;
+		this.numCards++;
+		if (this.getHandSum() >= 21) {
+			//this.endGame();
+		}
+		
+		return (this.getHandSum());
+	}
+	
+		//remove card
+	public boolean removeCard(Card aCard, int currentCard) {
+		this.hand[currentCard] = null;
+		for(int i = 0; i < numCards - 1; ++i) {
+			if(i < currentCard) {
+				continue;
+			}
+			else
+				this.hand[i] = this.hand[i + 1];
+		}
+		numCards -= 1;
+		return true;
+	}
+	
+		//splitting hand
+	public boolean canItSplit() {
+		int[] values = new int[numCards];
+		for (int i = 0; i < this.numCards; i++) {
+			values[i] = hand[i].getValue();
+		}
+		for (int j = 0; j < this.numCards; j++) {
+			if (values[j] == (values[j] = 1)) {
+				return true;
+			}
+			else
+				continue;
+		}
+		return false;
+	}
 }
